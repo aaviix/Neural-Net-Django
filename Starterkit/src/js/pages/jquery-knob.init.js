fef1315a92ae650355e0d2fdaa7588301580c296
aaviix <singhavanish8642@gmail.com>
2024-10-07 21:08:28 +0530

    first commit

---
+++ Starterkit/src/js/pages/jquery-knob.init.js
@@ -0,0 +1,11 @@
+/*
+Template Name: Veltrix - Admin & Dashboard Template
+Author: Themesbrand
+Website: https://themesdesign.in/
+Contact: themesbrand@gmail.com
+File: Jquery knob init Js File
+*/
+
+$(function() {
+    $(".knob").knob();
+});
